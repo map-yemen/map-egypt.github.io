7d0910258438a0c454ac75c7389dc04a5e02af6a
Olaf Veerman <olaf.veerman@gmail.com>
2018-11-27 14:56:55 -0500

    Fix email address and add another address

--- app/assets/scripts/components/page-footer.js
+++ app/assets/scripts/components/page-footer.js
@@ -53,7 +53,7 @@ var PageFooter = React.createClass({
             </div>
           </div>
           <div className='contact'>
-            <p className='contact__item'><a title='contact us' href='mailto:fpri-egypt@cgiar.org' className='link--primary'><span>{t.contact_link}</span></a> {t.contact_sentence}.</p>
+            <p className='contact__item'><a title='contact us' href='mailto:ifpri-egypt@cgiar.org, abdulwahedmukred@gmail.com' className='link--primary'><span>{t.contact_link}</span></a> {t.contact_sentence}.</p>
           </div>
         </div>
       </footer>
